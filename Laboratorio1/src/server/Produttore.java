9403d0a37086fa81104e1dc449694e981fa83b5c
Riccardo <ricky.roveri@gmail.com>
2021-10-09 09:45:34 +0200

    creato produttore

--- Laboratorio1/src/server/Produttore.java
+++ Laboratorio1/src/server/Produttore.java
@@ -1,9 +1,37 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Produttore {
 
-	public static void main(String[] args) {
-		System.out.println("produttore");
+	public static void main(String[] args) {		
+		
+		if (args.length != 1){
+			System.out.println("Utilizzo: produttore <inputFilename>");
+			System.exit(0);
+		}
+		
+		BufferedReader in = null;
+		in = new BufferedReader(new InputStreamReader(System.in));
+			
+		FileWriter fout;
+		String line = null;
+		try {
+			fout = new FileWriter(args[0]);
+			line = in.readLine() + "\n";
+			while (line != null) {
+				fout.write(line, 0, line.length());
+				line = in.readLine() + "\n";
+			}
+			fout.close();
+		}
+	    catch (IOException e) { 
+			e.printStackTrace();
+			System.exit(2); // uscita con errore, intero positivo a livello di sistema Unix
+		}
 	}
 
 }
