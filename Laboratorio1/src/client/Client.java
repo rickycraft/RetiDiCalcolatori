eccdfe3f5ef05ee359a5f77f387d96635df4cd42
hjcSteve <hustefano99@gmail.com>
2021-10-15 15:36:44 +0200

    cliente2.0

--- Laboratorio1/src/client/Client.java
+++ Laboratorio1/src/client/Client.java
@@ -2,9 +2,7 @@ package client;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
@@ -25,28 +23,22 @@ public class Client {
 			int port = Integer.parseInt(args[1]);
 			String filename = args[2];
 
-			// creazione datagramSocket
-			DatagramSocket socket = new DatagramSocket(port, address);
-			socket.setSoTimeout(300000);
-			byte[] buff = new byte[256];
-			// creazione pacchetto datagram
-			DatagramPacket packet = new DatagramPacket(buff, buff.length);
+			// creazione datagramSocket nel cliente
+			DatagramSocket socket = new DatagramSocket();
+			socket.setSoTimeout(3000);
 
-			// send discovery packet
-			// creazione del pacchetto con le informazione del nome file
-			ByteArrayOutputStream byteOutStream = new ByteArrayOutputStream();
-			DataOutputStream dataOutStream = new DataOutputStream(byteOutStream);
-			dataOutStream.writeUTF(filename); // nomeFile è una stringa
-			byte[] data = byteOutStream.toByteArray();
-			// riempimento del pacchetto
-			packet.setData(data, 0, data.length);
+			// creazione pacchetto datagram con nomeFile
+			byte[] sentData = new byte[256];
+			sentData = filename.getBytes();
+			DatagramPacket packet = new DatagramPacket(sentData, sentData.length, address, port);
 			// invio del pacchetto al server
 			socket.send(packet);
 
 			// inizializzaione del pacchetto risposta
-			packet.setData(buff);
-			// attesa del pacchetto di risposta
-			socket.receive(packet);
+			byte[] receiveData = new byte[256];
+			packet = new DatagramPacket(receiveData, receiveData.length);
+			socket.receive(packet); // attesa del pacchetto di risposta
+
 			// estrazione del pacchetto di ritorno
 			ByteArrayInputStream byteInStream = new ByteArrayInputStream(packet.getData(), 0, packet.getLength());
 			DataInputStream dataInStream = new DataInputStream(byteInStream);
@@ -60,61 +52,37 @@ public class Client {
 			}
 
 			// caso esito positivo posito ( numero porta)
-			// chiudo il socket DS
-			socket.close();
-			char si_no;
-			DatagramSocket socketRS = new DatagramSocket(rsPort, address);
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("numero di riga 1");
+			String numLinea1;
 
-			do {
-				// chiedo al terminale al cliente i numeri delle due righe
-
-				BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-				System.out.println("numero di riga 1");
-				int numLinea1 = Integer.parseInt(stdIn.readLine());
+			while ((numLinea1 = stdIn.readLine()) != null) {
 				System.out.println("numero di riga 2");
-				int numLinea2 = Integer.parseInt(stdIn.readLine());
+				String numLinea2 = stdIn.readLine();
+				byte[] sentData2 = new byte[256];
+				sentData2 = numLinea1.getBytes();
+				sentData2 = numLinea2.getBytes();
+				packet = new DatagramPacket(sentData2, sentData2.length, address, rsPort);
+				socket.send(packet);
 
-				// creazione datagramSocket RS (stesso indirizzo , diversa porta)
-				socket.setSoTimeout(300000);
-				byte[] buffRS = new byte[256];
-				// creazione pacchetto datagram RS
-				DatagramPacket packetRS = new DatagramPacket(buffRS, buffRS.length);
+				byte[] receiveData2 = new byte[256];
+				packet = new DatagramPacket(receiveData2, receiveData2.length);
+				socket.receive(packet);
 
-				// send RowSwap packet
-				// creazione del pacchetto con le informazione di NumRiga
-				// ???D
-				// ByteArrayOutputStream byteOutStream = new ByteArrayOutputStream();
-				// DataOutputStream dataOutStream = new DataOutputStream(byteOutStream);
-				dataOutStream.writeInt(numLinea1); // numRiga1
-				dataOutStream.writeInt(numLinea2); // numRiga2
-				byte[] dataRS = byteOutStream.toByteArray();
-				// riempimento del pacchetto RS
-				packetRS.setData(dataRS, 0, dataRS.length);
-				// invio del pacchetto al server RS
-				socketRS.send(packetRS);
-
-				// inizializzaione del pacchetto risposta
-				packet.setData(buffRS);
-				// attesa del pacchetto di risposta
-				socketRS.receive(packetRS);
-				// estrazione del pacchetto di ritorno
-				// ByteArrayInputStream byteInStream = new
-				// ByteArrayInputStream(packet.getData(), 0, packet.getLength());
-				// DataInputStream dataInStream = new DataInputStream(byteInStream);
-				// risposta del Row Swap Server ( intero )
+				byteInStream = new ByteArrayInputStream(packet.getData(), 0, packet.getLength());
+				dataInStream = new DataInputStream(byteInStream);
+				// risposta del RowSwap Server ( intero )
 				int rispRS = dataInStream.readInt();
-				// caso esito negativo da RS
 				if (rispRS < 0) {
 					System.out.println("errore: righe non scambiate");
 				}
+				System.out.println(" numero di riga 1");
+				numLinea1 = stdIn.readLine();
+			}
 
-				// chiedo al cliente se vuole fare una nuova richiesta
-				System.out.println("nuova richiesta??? (Y/N) ");
-				si_no = stdIn.readLine().charAt(0);
-			} while (si_no == 'y');
 			// finito il ciclo delle richieste
 			System.out.println("ho finito");
-			socketRS.close();
+			socket.close();
 			System.exit(0);
 		} catch (IOException e) {
 			e.printStackTrace();
