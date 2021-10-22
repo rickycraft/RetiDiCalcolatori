7588a26af20e99b9dad2ba8c96613581ab260457
hjcSteve <hustefano99@gmail.com>
2021-10-22 19:39:53 +0200

    Update FileUtility.java
    Update FileUtility
    da lettura ogni 1 byte alla volta 0--> lettura 1024 bytes alla volta

--- Laboratorio2/src/utility/FileUtility.java
+++ Laboratorio2/src/utility/FileUtility.java
@@ -11,17 +11,19 @@ public class FileUtility {
 	/**
 	 * Nota: sorgente e destinazione devono essere correttamente aperti e chiusi da
 	 * chi invoca questa funzione.
-	 * 
+	 *
 	 */
+	private static int BUFFER_SIZE = 1024;
 	public static void trasferisci_a_byte_file_binario(DataInputStream src, DataOutputStream dest) throws IOException {
 
 		// ciclo di lettura da sorgente e scrittura su destinazione
-		int buffer;
+		byte buffer[]=new byte[BUFFER_SIZE];
+		int nRead;
 		try {
 			// esco dal ciclo all lettura di un valore negativo -> EOF
 			// N.B.: la funzione consuma l'EOF
-			while ((buffer = src.read()) >= 0) {
-				dest.write(buffer);
+			while ((nRead =src.read(buffer,0,buffer.length) )>= 0) {
+				dest.write(buffer,0,nRead);
 			}
 			dest.flush();
 		} catch (EOFException e) {
