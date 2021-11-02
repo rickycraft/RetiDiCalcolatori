647133b3b9f86603bad55ea53c15fc3abdb43d9c
Riccardo <ricky.roveri@gmail.com>
2021-11-02 19:58:09 +0100

    fatto datagram_client/server

--- Laboratorio4/datagram_client.c
+++ Laboratorio4/datagram_client.c
@@ -1,105 +1,118 @@
-/* Client per richiedere il numero di file in un direttorio remoto */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <string.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define LENGTH_FILE_NAME 20
 
-int main(int argc, char **argv){
-	struct hostent *host;
-	struct sockaddr_in clientaddr, servaddr;
-	int sd, nread, port;
+int main(int argc, char **argv) {
+  struct hostent *host;
+  struct sockaddr_in clientaddr, servaddr;
+  int sd, port, result, len;
+  char nome_file[LENGTH_FILE_NAME], parola[LENGTH_FILE_NAME];
+  char *separator = "|";
 
-	int len, num_file;
-	char nome_dir[LENGTH_FILE_NAME];
+#pragma region setup
+  /* CONTROLLO ARGOMENTI ---------------------------------- */
+  if (argc != 3) {
+    printf("Error:%s serverAddress serverPort\n", argv[0]);
+    exit(1);
+  }
 
-	/* CONTROLLO ARGOMENTI ---------------------------------- */
-	if(argc!=3){
-		printf("Error:%s serverAddress serverPort\n", argv[0]);
-		exit(1);
-	}
+  /* INIZIALIZZAZIONE INDIRIZZO SERVER--------------------- */
+  memset((char *)&servaddr, 0, sizeof(struct sockaddr_in));
+  servaddr.sin_family = AF_INET;
+  host = gethostbyname(argv[1]);
+  if (host == NULL) {
+    printf("%s not found in /etc/hosts\n", argv[1]);
+    exit(2);
+  }
 
-	/* INIZIALIZZAZIONE INDIRIZZO SERVER--------------------- */
-	memset((char *)&servaddr, 0, sizeof(struct sockaddr_in));
-	servaddr.sin_family = AF_INET;
-	host = gethostbyname (argv[1]);
-	if (host == NULL){
-		printf("%s not found in /etc/hosts\n", argv[1]);
-		exit(2);
-	}
+  int nread = 0;
+  while (argv[2][nread] != '\0') {
+    if ((argv[2][nread] < '0') || (argv[2][nread] > '9')) {
+      printf("Secondo argomento non intero\n");
+      exit(2);
+    }
+    nread++;
+  }
+  port = atoi(argv[2]);
+  if (port < 1024 || port > 65535) {
+    printf("Porta scorretta...");
+    exit(2);
+  }
 
-	nread = 0;
-	while (argv[2][nread] != '\0'){
-		if ((argv[2][nread] < '0') || (argv[2][nread] > '9')){
-			printf("Secondo argomento non intero\n");
-			exit(2);
-		}
-		nread++;
-	}
-	port = atoi(argv[2]);
-	if (port < 1024 || port > 65535)
-	{printf("Porta scorretta...");exit(2);}
+  servaddr.sin_addr.s_addr = ((struct in_addr *)(host->h_addr))->s_addr;
+  servaddr.sin_port = htons(port);
 
-	servaddr.sin_addr.s_addr=((struct in_addr *)(host->h_addr))->s_addr;
-	servaddr.sin_port = htons(port);
+  /* INIZIALIZZAZIONE INDIRIZZO CLIENT--------------------- */
+  memset((char *)&clientaddr, 0, sizeof(struct sockaddr_in));
+  clientaddr.sin_family = AF_INET;
+  clientaddr.sin_addr.s_addr = INADDR_ANY;
+  clientaddr.sin_port = 0;
 
-	/* INIZIALIZZAZIONE INDIRIZZO CLIENT--------------------- */
-	memset((char *)&clientaddr, 0, sizeof(struct sockaddr_in));
-	clientaddr.sin_family = AF_INET;
-	clientaddr.sin_addr.s_addr = INADDR_ANY;  
-	clientaddr.sin_port = 0;
-	
-	printf("Client avviato\n");
+  printf("Client avviato\n");
 
-	/* CREAZIONE SOCKET ---------------------------- */
-	sd=socket(AF_INET, SOCK_DGRAM, 0);
-	if(sd<0) {perror("apertura socket"); exit(3);}
-	printf("Creata la socket sd=%d\n", sd);
-	
-	/* BIND SOCKET, a una porta scelta dal sistema --------------- */
-	if(bind(sd,(struct sockaddr *) &clientaddr, sizeof(clientaddr))<0)
-	{perror("bind socket "); exit(1);}
-	printf("Client: bind socket ok, alla porta %i\n", clientaddr.sin_port);
+  /* CREAZIONE SOCKET ---------------------------- */
+  sd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sd < 0) {
+    perror("apertura socket");
+    exit(3);
+  }
+  printf("Creata la socket sd=%d\n", sd);
 
-	/* CORPO DEL CLIENT: */
-	printf("Nome del direttorio: ");
+  /* BIND SOCKET, a una porta scelta dal sistema --------------- */
+  if (bind(sd, (struct sockaddr *)&clientaddr, sizeof(clientaddr)) < 0) {
+    perror("bind socket ");
+    exit(1);
+  }
+  printf("Client: bind socket ok, alla porta %i\n", clientaddr.sin_port);
+#pragma endregion
+  /*
+  chiede ciclicamente all’utente il nome del file
+  e la parola, invia al server la richiesta di eliminazione, e
+  attende il pacchetto con l’esito dell’operazione, cioè il
+  numero di eliminazioni della parola che stampa a video
+  Il file è modificato nel servitore
+  */
+  do {
+    printf("Nome del file: ");
+    if (gets(nome_file) == 0) break;
+    printf("Parola da eliminare: ");
+    if (gets(parola) == 0) break;
 
-	while (gets(nome_dir)){
-		/* invio richiesta */
-		len=sizeof(servaddr);
-		if (sendto(sd, nome_dir, (strlen(nome_dir)+1), 0, (struct sockaddr *)&servaddr, len)<0){
-			perror("scrittura socket");
-			printf("Nome del direttorio: ");
-			continue; // se questo invio fallisce il client torna all'inzio del ciclo
-		}
+    len = sizeof(servaddr);
+    // nome_file|parola
+    if (sendto(sd, strcat(strcat(nome_file, separator), parola),
+               (strlen(nome_file) + strlen(parola) + 2), 0,
+               (struct sockaddr *)&servaddr, len) < 0) {
+      perror("sendto");
+      continue;
+    }
 
-		/* ricezione del risultato */
-		printf("Attesa del risultato...\n");
-		if (recvfrom(sd, &num_file, sizeof(num_file), 0, (struct sockaddr *)&servaddr, &len)<0){
-			perror("recvfrom");
-			printf("Nome del direttorio: ");
-			continue; // se questa ricezione fallisce il client torna all'inzio del ciclo
-		}
+    if (recvfrom(sd, &result, sizeof(result), 0, (struct sockaddr *)&servaddr,
+                 &len) < 0) {
+      perror("recvfrom");
+      continue;
+    }
 
-		if (num_file<0) printf("Il direttorio %s è scorretto o non esiste\n", nome_dir);
+    if (result < 1) {
+      printf("Errore del server");
+    } else {
+      printf("Numero di parole eliminate %d\n", result);
+    }
 
-		else printf("Nel direttorio %s ci sono %d file: %u\n", nome_dir, ntohl(num_file));
-		
-		printf("Nome del direttorio: ");
+  } while (1);
 
-	} // while
-
-	printf("\nClient: termino...\n");
-	shutdown(sd,0);
-	shutdown(sd,1);
-	close(sd);
-	exit(0);
+  printf("\nClient: termino...\n");
+  shutdown(sd, 0);
+  shutdown(sd, 1);
+  close(sd);
+  exit(0);
 }
--- Laboratorio4/select_server.c
+++ Laboratorio4/select_server.c
@@ -16,233 +16,200 @@
 #define LENGTH_FILE_NAME 20
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
-/********************************************************/
-int conta_file(char *name)
-{
-	DIR *dir;
-	struct dirent *dd;
-	int count = 0;
-	dir = opendir(name);
-	if (dir == NULL)
-		return -1;
-	while ((dd = readdir(dir)) != NULL)
-	{
-		printf("Trovato il file %s\n", dd->d_name);
-		count++;
-	}
-	printf("Numero totale di file %d\n", count);
-	closedir(dir);
-	return count;
-}
-/********************************************************/
-void gestore(int signo)
-{
-	int stato;
-	printf("esecuzione gestore di SIGCHLD\n");
-	wait(&stato);
-}
-/********************************************************/
+int elimina_parole_dafile(char *nome_file, char *parola) { return 1; }
 
-int main(int argc, char **argv)
-{
-	int listenfd, connfd, udpfd, fd_file, nready, maxfdp1;
-	const int on = 1;
-	char zero = 0, buff[DIM_BUFF], datagram_in[2 * LENGTH_FILE_NAME], nome_directory[LENGTH_FILE_NAME];
-	fd_set rset;
-	int len, nread, nwrite, num, ris, port;
-	struct sockaddr_in cliaddr, servaddr;
+void gestore(int signo) {
+  int stato;
+  printf("esecuzione gestore di SIGCHLD\n");
+  wait(&stato);
+}
+
+int main(int argc, char **argv) {
+  int listenfd, connfd, udpfd, fd_file, nready, maxfdp1;
+  const int on = 1;
+  char zero = 0, buff[DIM_BUFF], packet_in[2 * LENGTH_FILE_NAME];
+  char *nome_file, *parola;
+  const char *separator = "|";
+  DIR *dir;
+  struct dirent *dd;
+  fd_set rset;
+  int len, nread, nwrite, num, ris, port;
+  struct sockaddr_in cliaddr, servaddr;
 #pragma region controlli
-	/* CONTROLLO ARGOMENTI ---------------------------------- */
-	if (argc != 2)
-	{
-		printf("Error: %s port\n", argv[0]);
-		exit(1);
-	}
-	nread = 0;
-	while (argv[1][nread] != '\0')
-	{
-		if ((argv[1][nread] < '0') || (argv[1][nread] > '9'))
-		{
-			printf("Terzo argomento non intero\n");
-			exit(2);
-		}
-		nread++;
-	}
-	port = atoi(argv[1]);
-	if (port < 1024 || port > 65535)
-	{
-		printf("Porta scorretta...");
-		exit(2);
-	}
+  /* CONTROLLO ARGOMENTI ---------------------------------- */
+  if (argc != 2) {
+    printf("Error: %s port\n", argv[0]);
+    exit(1);
+  }
+  nread = 0;
+  while (argv[1][nread] != '\0') {
+    if ((argv[1][nread] < '0') || (argv[1][nread] > '9')) {
+      printf("Terzo argomento non intero\n");
+      exit(2);
+    }
+    nread++;
+  }
+  port = atoi(argv[1]);
+  if (port < 1024 || port > 65535) {
+    printf("Porta scorretta...");
+    exit(2);
+  }
 
-	/* INIZIALIZZAZIONE INDIRIZZO SERVER ----------------------------------------- */
-	memset((char *)&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = INADDR_ANY;
-	servaddr.sin_port = htons(port);
+  /* INIZIALIZZAZIONE INDIRIZZO SERVER -----------------------------------------
+   */
+  memset((char *)&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = INADDR_ANY;
+  servaddr.sin_port = htons(port);
 
-	printf("Server avviato\n");
+  printf("Server avviato\n");
 
-	/* CREAZIONE SOCKET TCP ------------------------------------------------------ */
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (listenfd < 0)
-	{
-		perror("apertura socket TCP ");
-		exit(1);
-	}
-	printf("Creata la socket TCP d'ascolto, fd=%d\n", listenfd);
+  /* CREAZIONE SOCKET TCP ------------------------------------------------------
+   */
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (listenfd < 0) {
+    perror("apertura socket TCP ");
+    exit(1);
+  }
+  printf("Creata la socket TCP d'ascolto, fd=%d\n", listenfd);
 
-	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
-	{
-		perror("set opzioni socket TCP");
-		exit(2);
-	}
-	printf("Set opzioni socket TCP ok\n");
+  if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
+    perror("set opzioni socket TCP");
+    exit(2);
+  }
+  printf("Set opzioni socket TCP ok\n");
 
-	if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-	{
-		perror("bind socket TCP");
-		exit(3);
-	}
-	printf("Bind socket TCP ok\n");
+  if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+    perror("bind socket TCP");
+    exit(3);
+  }
+  printf("Bind socket TCP ok\n");
 
-	if (listen(listenfd, 5) < 0)
-	{
-		perror("listen");
-		exit(4);
-	}
-	printf("Listen ok\n");
+  if (listen(listenfd, 5) < 0) {
+    perror("listen");
+    exit(4);
+  }
+  printf("Listen ok\n");
 
-	/* CREAZIONE SOCKET UDP ------------------------------------------------ */
-	udpfd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (udpfd < 0)
-	{
-		perror("apertura socket UDP");
-		exit(5);
-	}
-	printf("Creata la socket UDP, fd=%d\n", udpfd);
+  /* CREAZIONE SOCKET UDP ------------------------------------------------ */
+  udpfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (udpfd < 0) {
+    perror("apertura socket UDP");
+    exit(5);
+  }
+  printf("Creata la socket UDP, fd=%d\n", udpfd);
 
-	if (setsockopt(udpfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
-	{
-		perror("set opzioni socket UDP");
-		exit(6);
-	}
-	printf("Set opzioni socket UDP ok\n");
+  if (setsockopt(udpfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
+    perror("set opzioni socket UDP");
+    exit(6);
+  }
+  printf("Set opzioni socket UDP ok\n");
 
-	if (bind(udpfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-	{
-		perror("bind socket UDP");
-		exit(7);
-	}
-	printf("Bind socket UDP ok\n");
+  if (bind(udpfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+    perror("bind socket UDP");
+    exit(7);
+  }
+  printf("Bind socket UDP ok\n");
 
-	/* AGGANCIO GESTORE PER EVITARE FIGLI ZOMBIE -------------------------------- */
-	signal(SIGCHLD, gestore);
+  /* AGGANCIO GESTORE PER EVITARE FIGLI ZOMBIE --------------------------------
+   */
+  signal(SIGCHLD, gestore);
 #pragma endregion controlli
-	/* PULIZIA E SETTAGGIO MASCHERA DEI FILE DESCRIPTOR ------------------------- */
-	FD_ZERO(&rset);
-	maxfdp1 = max(listenfd, udpfd) + 1;
+  /* PULIZIA E SETTAGGIO MASCHERA DEI FILE DESCRIPTOR --------------------*/
+  FD_ZERO(&rset);
+  maxfdp1 = max(listenfd, udpfd) + 1;
 
-	/* CICLO DI RICEZIONE EVENTI DALLA SELECT ----------------------------------- */
-	for (;;)
-	{
-		FD_SET(listenfd, &rset);
-		FD_SET(udpfd, &rset);
+  /* CICLO DI RICEZIONE EVENTI DALLA SELECT -------------------------------*/
+  for (;;) {
+    FD_SET(listenfd, &rset);
+    FD_SET(udpfd, &rset);
 
-		if ((nready = select(maxfdp1, &rset, NULL, NULL, NULL)) < 0)
-		{
-			if (errno == EINTR)
-				continue;
-			else
-			{
-				perror("select");
-				exit(8);
-			}
-		}
+    if ((nready = select(maxfdp1, &rset, NULL, NULL, NULL)) < 0) {
+      if (errno == EINTR)
+        continue;
+      else {
+        perror("select");
+        exit(8);
+      }
+    }
 
-		/* GESTIONE TCP ------------------------------------- */
-		if (FD_ISSET(listenfd, &rset))
-		{
-			/*
-			Il server riceve il nome del direttorio; se il direttorio esiste
-			restituisce il nome dei file contenuti in tutti i direttori di secondo livello
-			in caso di errore notifica il cliente ma tiene la connessione aperta
-			*/
-			printf("Ricevuta richiesta\n");
-			len = sizeof(struct sockaddr_in);
-			if ((connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &len)) < 0)
-			{
-				if (errno == EINTR)
-					continue;
-				else
-				{
-					perror("accept");
-					exit(9);
-				}
-			}
+    /* GESTIONE TCP ------------------------------------- */
+    if (FD_ISSET(listenfd, &rset)) {
+      /*
+      Il server riceve il nome del direttorio; se il direttorio esiste
+      restituisce il nome dei file contenuti in tutti i direttori di secondo
+      livello in caso di errore notifica il cliente
+      */
+      printf("Ricevuta richiesta\n");
+      len = sizeof(struct sockaddr_in);
+      if ((connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &len)) < 0) {
+        if (errno == EINTR)
+          continue;
+        else {
+          perror("accept");
+          exit(9);
+        }
+      }
 
-			if (fork() == 0)
-			{
-				close(listenfd);
-				printf("Dentro il figlio, pid=%i\n", getpid());
+      if (fork() == 0) {
+        close(listenfd);
+        printf("Dentro il figlio, pid=%i\n", getpid());
 
-				for (;;)
-				{
-					if ((read(connfd, &nome_directory, sizeof(nome_directory))) <= 0)
-					{
-						perror("read");
-						break;
-					}
-					printf("Richiesta directory %s\n", nome_directory);
+        for (;;) {
+          if ((read(connfd, &packet_in, sizeof(packet_in))) <= 0) {
+            perror("read");
+            break;
+          }
+          printf("Richiesta directory %s\n", packet_in);
 
-					// leggo i file e mando il nome dei file, ho la connessione
+          dir = opendir(packet_in);
+          if (dir == NULL) {
+            // errore nella directory
+            continue;
+          }
+          while ((dd = readdir(dir)) != NULL) {
+            printf("Trovato il file %s\n", dd->d_name);
+            write(connfd, dd->d_name, sizeof(dd->d_name));
+          }
+          closedir(dir);
 
-					printf("Terminato invio nomi file\n");
+          printf("Terminato invio nomi file\n");
+        }
+        printf("Figlio %i: chiudo connessione e termino\n", getpid());
+        close(connfd);
+        exit(0);
+      }
+      close(connfd);
+    }
 
-					/* 
-					invio al client segnale di terminazione: zero binario
-					write(connfd, &zero, 1);
-					close(fd_file);
-					non serve
-					*/
-				}
-			}
-			printf("Figlio %i: chiudo connessione e termino\n", getpid());
-			close(connfd);
-			exit(0);
-		}
+    /* GESTIONE UDP ------------------------------------------ */
+    if (FD_ISSET(udpfd, &rset)) {
+      /*
+      Il server riceve il datagramma con il nome del file e la parola:
+      se il file esiste, elimina le occorrenze della parola all’interno del
+      file e invia al client l’intero positivo corrispondente alle eliminazioni
+      fatte in caso di errore, invia un intero negativo
+      */
+      printf("Server: ricevuta richiesta di conteggio file\n");
+      len = sizeof(struct sockaddr_in);
+      num = -1;
+      if (recvfrom(udpfd, &packet_in, sizeof(packet_in), 0,
+                   (struct sockaddr *)&cliaddr, &len) < 0) {
+        perror("recvfrom");
+        continue;
+      }
 
-		/* GESTIONE UDP ------------------------------------------ */
-		if (FD_ISSET(udpfd, &rset))
-		{
-			/* 
-			Il server riceve il datagramma con il nome del file e la parola:
-			se il file esiste, elimina le occorrenze della parola all’interno del
-			file e invia al client l’intero positivo corrispondente alle eliminazioni
-			fatte in caso di errore, invia un intero negativo
-			*/
-			printf("Server: ricevuta richiesta di conteggio file\n");
-			len = sizeof(struct sockaddr_in);
-			num = -1;
-			if (recvfrom(udpfd, &datagram_in, sizeof(datagram_in), 0, (struct sockaddr *)&cliaddr, &len) < 0)
-			{
-				perror("recvfrom");
-				continue;
-			}
-
-			printf("Ricevuto datagram %s\n", datagram_in);
-			// split datagram_in nomefile|parola
-
-			// num = eliminaparoleinfile(nomefile, parola)
-
-			printf("Risultato del conteggio: %i\n", num);
-			ris = htonl(num);
-			if (sendto(udpfd, &ris, sizeof(ris), 0, (struct sockaddr *)&cliaddr, len) < 0)
-			{
-				perror("sendto");
-				continue;
-			}
-
-		} /* fine gestione richieste di conteggio */
-	}		/* ciclo for della select */
+      nome_file = strtok(packet_in, separator);
+      parola = strtok(NULL, separator);
+      printf("Ricevuto datagram %s|%s\n", nome_file, parola);
+      num = elimina_parole_dafile(nome_file, parola);
+      printf("Risultato del conteggio: %i\n", num);
+      // ris = htonl(num);
+      if (sendto(udpfd, &num, sizeof(num), 0, (struct sockaddr *)&cliaddr,
+                 len) < 0) {
+        perror("sendto");
+        continue;
+      }
+    }
+  }
 }
