880a8b371637dd50291c84feaa0c70bf4e7ceae1
GiusiAlbanese <albanese.giusi@outlook.it>
2021-11-03 16:04:19 +0100

    Update stream_client.c

--- Laboratorio4/stream_client.c
+++ Laboratorio4/stream_client.c
@@ -1,130 +1,89 @@
-/* Client per richiedere l'invio di un file (get, versione 1) */
+/* Client per richiedere la lista di nomi di file remoti presenti 
+nei direttori di secondo livello */
 
-#include <fcntl.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
 #define DIM_BUFF 100
-#define LENGTH_FILE_NAME 20
+#define LENGTH_DIRECTORY_NAME 20
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	int sd, nread, port;
-	char c, ok, nome_file[LENGTH_FILE_NAME];
+	char c, nome_direttorio[LENGTH_DIRECTORY_NAME];
 	struct hostent *host;
-	struct sockaddr_in servaddr;
-#pragma region controlli
-	/* CONTROLLO ARGOMENTI ---------------------------------- */
-	if (argc != 3)
-	{
+	struct sockaddr_in servaddr;  
+
+	/* CONTROLLO NUMERO ARGOMENTI */
+	if(argc!=3){
 		printf("Error:%s serverAddress serverPort\n", argv[0]);
 		exit(1);
 	}
 	printf("Client avviato\n");
 
-	/* PREPARAZIONE INDIRIZZO SERVER ----------------------------- */
+	/* PREPARAZIONE INDIRIZZO SERVER */
 	memset((char *)&servaddr, 0, sizeof(struct sockaddr_in));
 	servaddr.sin_family = AF_INET;
 	host = gethostbyname(argv[1]);
-	if (host == NULL)
-	{
+	/*GESTIONE ERRORE */
+	if (host == NULL){
 		printf("%s not found in /etc/hosts\n", argv[1]);
 		exit(2);
 	}
 
+	/*CONTROLLO SECONDO ARGOMENTO: PORTA */
 	nread = 0;
-	while (argv[2][nread] != '\0')
-	{
-		if ((argv[2][nread] < '0') || (argv[2][nread] > '9'))
-		{
+	while (argv[2][nread] != '\0'){
+		if ((argv[2][nread] < '0') || (argv[2][nread] > '9')){
 			printf("Secondo argomento non intero\n");
 			exit(2);
 		}
 		nread++;
 	}
 	port = atoi(argv[2]);
+	/*GESTIONE ERRORE*/
 	if (port < 1024 || port > 65535)
-	{
-		printf("Porta scorretta...");
-		exit(2);
-	}
+	{printf("Porta scorretta...");exit(2);}
 
-	servaddr.sin_addr.s_addr = ((struct in_addr *)(host->h_addr))->s_addr;
+	servaddr.sin_addr.s_addr=((struct in_addr*) (host->h_addr))->s_addr;
 	servaddr.sin_port = htons(port);
 
-	/* CREAZIONE E CONNESSIONE SOCKET (BIND IMPLICITA) ----------------- */
-	sd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sd < 0)
-	{
-		perror("apertura socket ");
-		exit(3);
-	}
+	/* CREAZIONE E CONNESSIONE SOCKET (BIND IMPLICITA)*/
+	sd=socket(AF_INET, SOCK_STREAM, 0);
+	if (sd <0){perror("apertura socket "); exit(3);}
 	printf("Creata la socket sd=%d\n", sd);
 
-	if (connect(sd, (struct sockaddr *)&servaddr, sizeof(struct sockaddr)) < 0)
-	{
-		perror("Errore in connect");
-		exit(4);
-	}
+	/* GESTIONE ERRORE */
+	if (connect(sd,(struct sockaddr *) &servaddr, sizeof(struct sockaddr))<0)
+	{perror("Errore in connect"); exit(4);}
 	printf("Connect ok\n");
-#pragma endregion
 
-	/*
-chiede ciclicamente all’utente il nome del file
-e la parola, invia al server la richiesta di eliminazione, e
-attende il pacchetto con l’esito dell’operazione, cioè il
-numero di eliminazioni della parola che stampa a video
-Il file è modificato nel servitore
-*/
-printf("Inserisci il nome del file e la parola da eliminare, entrambi separati da uno spazio\n");
+	/* CORPO DEL CLIENT */
+	printf("Nome del direttorio: ");
 
-	while (gets(nome_file))
-	{
+	while (gets(nome_direttorio)){
 
-		if (write(sd, nome_file, (strlen(nome_file) + 1)) < 0)
-		{
+		/*GESTIONE ERRORE INVIO*/
+		if (write(sd, nome_direttorio, (strlen(nome_direttorio)+1))<0){
 			perror("write");
 			break;
 		}
+		printf("Richiesta del direttorio %s inviata... \n", nome_direttorio);
 
-		if (read(sd, &ok, 1) < 0)
-		{
-			perror("read");
-			break;
-		}
+		printf("Ricevo la lista di nomi:\n");
+		
 
-		if (ok == 'S')
-		{
-			printf("Ricevo il file:\n");
-			while ((nread = read(sd, &c, 1)) > 0) // leggo a caratteri per individuare il fine file
-				if (c != '\0')
-				{
-					write(1, &c, 1);
-				}
-				else
-					break;
-			if (nread < 0)
-			{
-				perror("read");
-				break;
-			}
-		}
-		else if (ok == 'N')
-			printf("File inesistente\n");
-		else
-			printf("Errore di protocollo\n"); //controllare sempre che il protocollo sia rispettato
-
-		printf("Nome del file da richiedere: ");
-	} //while
+		printf("Nome del direttorio: ");
+	}//while
 	printf("\nClient: termino...\n");
-	shutdown(sd, 0);
-	shutdown(sd, 1);
+	shutdown(sd,0);
+	shutdown(sd,1);
 	close(sd);
 	exit(0);
 }
