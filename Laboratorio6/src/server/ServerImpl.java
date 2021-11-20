6b63137c3a5459d223c40a14b95c81a509fcbada
hjcSteve <hustefano99@gmail.com>
2021-11-20 12:16:26 +0100

    eliminaRigadaFile(funzionante)
    Attenzione inizia con indice 0 e bisogna indicarlo anche nel cliente
    provato in locale

--- Laboratorio6/src/server/ServerImpl.java
+++ Laboratorio6/src/server/ServerImpl.java
@@ -3,6 +3,7 @@ package server;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -118,10 +119,58 @@ public class ServerImpl extends UnicastRemoteObject implements RemOp {
 		}else {
 			return new FileRighe(nomeFile,righeNuove);
 		}
-		
-		
-		
-
-
 	}
+	public FileRighe elimina_riga2(String nomeFile,int numRigaElim) {
+		int indiceRiga=0;
+		int righeFile=0;
+		
+		String lineaLetta;
+		try {
+			BufferedReader rd=new BufferedReader(new FileReader(nomeFile));
+			System.out.println("File aperto");
+
+			File fileTemp=new File(nomeFile+"_temp");
+			BufferedWriter wr=new BufferedWriter(new FileWriter(fileTemp));
+			System.out.println("FileTenmpCreato");
+
+		
+			while((lineaLetta = rd.readLine())!=null) {
+				if (indiceRiga!=numRigaElim) {
+					wr.write(lineaLetta+"\n");
+					System.out.println("riga "+indiceRiga);
+
+					righeFile++;
+				}
+				indiceRiga++;
+			}
+			System.out.println(indiceRiga);
+
+			rd.close();
+			wr.close();
+			if(indiceRiga<=numRigaElim) {
+				fileTemp.delete();
+				
+				throw new RemoteException("non esiste riga numero "+numRigaElim+" nel file "+nomeFile);
+			}else {
+				File fileOg=new File(nomeFile);
+				fileOg.delete();
+				
+
+				fileTemp.renameTo(fileOg);
+				System.out.println("file sostituito");
+				return new FileRighe(nomeFile,righeFile);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}	
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	
+	
 }
