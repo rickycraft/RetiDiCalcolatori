f417945b1e6be19259be84b77ff41dbfc1ca4305
Riccardo <ricky.roveri@gmail.com>
2021-12-04 13:00:47 +0100

    laboratorio8 funzionante

--- Laboratorio8/compile.sh
+++ Laboratorio8/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gcc es8_client.c operazioni_xdr.c operazioni_clnt.c -o client.bin
+gcc es8_client.c operazioni_xdr.c operazioni_clnt.c -o es8_client.bin -g
 gcc operazioni_xdr.c operazioni_svc.c es8_proc.c -o server.bin
--- Laboratorio8/es8_client.c
+++ Laboratorio8/es8_client.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 #include "operazioni.h"
+#define BUFF_DIM 100
 
 int main(int argc, char *argv[]) {
   CLIENT *cl;
   char *server;
+  char *buff1, *buff2;
+  buff1 = malloc(BUFF_DIM * sizeof(char));
+  buff2 = malloc(BUFF_DIM * sizeof(char));
+  char c[10];
   if (argc < 2) {
     exit(1);
   }
@@ -15,11 +20,36 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  Wc *wc = file_scan_1(&argv[2], cl);
-  NameSize namesize = {argv[3], atoi(argv[4])};
-  printf("name: %s, size: %d\n", namesize.nome, namesize.dim);
-  int *count = dir_scan_1(&namesize, cl);
-
-  printf("l: %d, w: %d, c: %d, count: %d\n", wc->righe, wc->parole,
-         wc->caratteri, *count);
+  printf("che programma vuoi usare?\nfile_scan (f) | dir_scan (d)\n");
+  while (gets(c)) {
+    if (c[0] == 'f') {
+      // file scan
+      printf("Digita il nome del file: ");
+      fgets(buff1, BUFF_DIM, stdin);
+      Wc *wc = file_scan_1(&buff1, cl);
+      if (wc == NULL) {
+        fprintf(stderr, "%s: %s fallisce la rpc n\n", argv[0], server);
+        clnt_perror(cl, server);
+      } else {
+        printf("l: %d, p: %d, c: %d\n", wc->righe, wc->parole, wc->caratteri);
+      }
+    } else if (c[0] == 'd') {
+      // dir scan
+      printf("Digita il nome della directory: \n");
+      gets(buff1);
+      printf("Digita la soglia minima: \n");
+      gets(buff2);
+      NameSize namesize = {buff1, atoi(buff2)};
+      int *count = dir_scan_1(&namesize, cl);
+      if (count == NULL) {
+        fprintf(stderr, "%s: %s fallisce la rpc n\n", argv[0], server);
+        clnt_perror(cl, server);
+      } else {
+        printf("n_file sopra soglia: %d\n", *count);
+      }
+    }
+    printf("che programma vuoi usare?\nfile_scan (f) | dir_scan (d)\n");
+  }
+  free(buff1);
+  free(buff2);
 }
